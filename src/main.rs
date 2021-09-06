5e121ac64c25b61cef1eefa100e5491feb6278cc
Shelby Doolittle <shelby@shelbyd.com>
2021-09-06 16:36:18 -0400

    improve performance by not parsing derivation each loop

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use concread::CowCell;
-use rand::Rng;
-use rayon::iter::ParallelIterator;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use sp_core::{
     crypto::{AccountId32, Ss58AddressFormat, Ss58Codec},
     sr25519::Pair,
-    Pair as PairTrait,
+    DeriveJunction, Pair as PairTrait,
 };
 use structopt::StructOpt;
 
-use std::sync::{mpsc, Arc};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    mpsc, Arc,
+};
 
 #[derive(StructOpt, Debug)]
 struct Options {
@@ -77,11 +79,8 @@ impl Options {
             .count()
     }
 
-    fn is_perfect_match(&self, candidate: &Option<Candidate>) -> bool {
-        match candidate {
-            None => false,
-            Some(c) => c.address.starts_with(&self.prefix),
-        }
+    fn is_perfect_match(&self, candidate: &Candidate) -> bool {
+        candidate.address.starts_with(&self.prefix)
     }
 }
 
@@ -92,6 +91,8 @@ fn matching_prefix_length(a: &str, b: &str) -> usize {
 fn main() {
     let options = Arc::new(Options::from_args());
 
+    let should_continue = Arc::new(AtomicBool::new(true));
+
     let best_so_far = Arc::new(CowCell::new(None));
     let (better_tx, better_rx) = mpsc::sync_channel(10);
 
@@ -99,23 +100,35 @@ fn main() {
         let best_so_far = best_so_far.clone();
         let better_tx = better_tx.clone();
         let options = options.clone();
+        let should_continue = should_continue.clone();
         std::thread::spawn(move || {
-            rayon::iter::repeat(())
-                .map(|_| match options.is_perfect_match(&best_so_far.read()) {
-                    true => None,
-                    false => Some(()),
+            let base_secret = rand::random::<[u64; 3]>();
+            let base_seed = format!(
+                "{}/{}/{}/{}",
+                &options.seed_prefix, base_secret[0], base_secret[1], base_secret[2]
+            );
+            let base_pair = Pair::from_string(&base_seed, None).unwrap();
+
+            let n_offset = rand::random::<u64>();
+            (0..u64::MAX)
+                .into_par_iter()
+                .map(|n| match should_continue.load(Ordering::Relaxed) {
+                    true => Some(n.wrapping_add(n_offset)),
+                    false => None,
                 })
                 .while_some()
-                .map(|_| {
-                    let mut secret = [0; 32];
-                    rand::thread_rng().fill(&mut secret);
-                    let seed = format!("{}/{}", options.seed_prefix, hex::encode(secret));
-
-                    let pair = Pair::from_string(&seed, None).unwrap();
-                    (pair, seed)
+                .map(|n| {
+                    let derived = base_pair
+                        .derive(core::iter::once(DeriveJunction::soft(n)), None)
+                        .unwrap()
+                        .0;
+                    (derived, n)
                 })
                 .filter(|pair| options.pair_is_better(&pair.0, &best_so_far.read()))
-                .for_each(|pair| better_tx.send(pair).unwrap());
+                .for_each(|(pair, n)| {
+                    let seed = format!("{}/{}", base_seed, n);
+                    better_tx.send((pair, seed)).unwrap()
+                });
         })
     };
     drop(better_tx);
@@ -126,6 +139,10 @@ fn main() {
         .for_each(|pair| {
             let candidate = options.to_candidate(pair);
             candidate.display();
+
+            if options.is_perfect_match(&candidate) {
+                should_continue.store(false, Ordering::Relaxed);
+            }
 
             let mut write_txn = best_so_far.write();
             *write_txn = Some(candidate);
@@ -143,6 +160,6 @@ struct Candidate {
 
 impl Candidate {
     fn display(&self) {
-        println!("{} -> {}", self.address, self.seed);
+        println!("{}\n    {}", self.address, self.seed);
     }
 }
